d511d80eb360bfaf2f22f76c1f9807dd67c09555
gabdevaraujo <biel.vianadearaujo@gmail.com>
2026-03-12 08:39:14 -0300

    Projeto criado

---
+++ src/main/java/br/com/gva/imobfacil/ImobfacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.gva.imobfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobfacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobfacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gva/imobfacil/ImobfacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gva.imobfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobfacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
